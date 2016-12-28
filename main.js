6ca3901b286a824eb8d4b15d2729e694fb700e90
Александр <forjob159@gmail.com>
2016-12-29 01:51:22 +0500

    add: app icon, npm build script, menu design

--- main.js
+++ main.js
@@ -3,9 +3,99 @@ const path = require('path')
 const app = electron.app
 const Menu = electron.Menu;
 const BrowserWindow = electron.BrowserWindow
-//const Tray = electron.Tray;
 
 let mainWindow = null;
+
+
+const template = [
+  {
+    label: 'Edit',
+    submenu: [
+      { role: 'copy' },
+      { role: 'selectall' }
+    ]
+  },
+  {
+    label: 'View',
+    submenu: [
+      { role: 'resetzoom' },
+      { role: 'zoomin' },
+      { role: 'zoomout' },
+      { type: 'separator' },
+      { role: 'togglefullscreen' }
+    ]
+  },
+  {
+    role: 'window',
+    submenu: [
+      { role: 'minimize' },
+      { role: 'close' }
+    ]
+  },
+  {
+    role: 'help',
+    submenu: [
+      {
+        label: 'Report an issue',
+        click () {
+          require('electron')
+            .shell
+            .openExternal('https://github.com/bespoyasov/kursovik-redux-electron/issues')
+        }
+      }
+    ]
+  }
+]
+
+if (process.platform === 'darwin') {
+  template.unshift({
+    label: app.getName(),
+    submenu: [
+      { role: 'about' },
+      { type: 'separator' },
+      { role: 'hide' },
+      { role: 'hideothers' },
+      { role: 'unhide' },
+      { type: 'separator' },
+      { role: 'quit' }
+    ]
+  })
+  //Edit menu.
+  template[1].submenu.push(
+    { type: 'separator' },
+    { label: 'Speech',
+      submenu: [
+        { role: 'startspeaking' },
+        { role: 'stopspeaking' }
+      ]
+    }
+  )
+  // Window menu.
+  template[3].submenu = [
+    {
+      label: 'Close',
+      accelerator: 'CmdOrCtrl+W',
+      role: 'close'
+    },
+    {
+      label: 'Minimize',
+      accelerator: 'CmdOrCtrl+M',
+      role: 'minimize'
+    },
+    {
+      label: 'Zoom',
+      role: 'zoom'
+    },
+    {
+      type: 'separator'
+    },
+    {
+      label: 'Bring All to Front',
+      role: 'front'
+    }
+  ]
+}
+
 
 
 require('electron-context-menu')({
@@ -20,6 +110,9 @@ require('electron-context-menu')({
 
 
 function createWindow () {
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
+
   mainWindow = new BrowserWindow({
     width: 560,
     height: 660,
@@ -27,21 +120,12 @@ function createWindow () {
     icon: path.join(__dirname, 'assets/app-icon.png')
   })
 
-  //mainWindow.loadURL(`file://${__dirname}/public/index.html`)
-  mainWindow.loadURL(`http://localhost:8080`);
+  mainWindow.loadURL(`file://${__dirname}/public/index.html`)
+  //mainWindow.loadURL(`http://localhost:8080`);
 
   mainWindow.on('closed', function () {
     mainWindow = null
   });
-
-  // if (!tray) {
-  //   tray = new Tray(path.join(__dirname, 'assets/tray-icon.png'))
-  //   tray.setToolTip('Открыть Курсовик')
-  //   tray.on('click', () => {
-  //     if (mainWindow !== null) mainWindow.show();
-  //     else createWindow()
-  //   })
-  // }
 }
 
 
